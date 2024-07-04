f3d4e444e2947f18f08246d8a88473c4990f4d94
PM-Devs <princegh100@outlook.com>
2024-07-04 15:22:02 +0000

    All the

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,15 @@ const authMiddleware = require('../middelware/authMiddleware.js');
 const validationMiddleware = require('../middelware/validationMiddleware.js');
 const { check } = require('express-validator');
 
+
+
 // Create a new user
 router.post('/users', [
-    check('email').isEmail(),
-    check('password').isLength({ min: 6 })
+    check('name').not().isEmpty().withMessage('Name is required'),
+    check('email').isEmail().withMessage('Invalid email address'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    check('role').isIn(['Student', 'Supervisor', 'Admin']).withMessage('Role must be one of Student, Supervisor, or Admin')
 ], validationMiddleware, userController.createUser);
-
 // Get all users
 router.get('/users', authMiddleware, userController.getAllUsers);
 
